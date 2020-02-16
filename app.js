614e3809882451f417b5a6e2239ccae6d2aab4cd
Rémi <ponche62880@gmail.com>
2020-02-17 00:15:35 +0100

    move the route to the router folder
    reoganize the code

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const secret = require('./secret/MongoDb'); 
-const Thing = require('./models/Thing');
+
+const stuffRoute =require('./routes/stuff'); 
 
 const app = express();
 
@@ -23,40 +24,9 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id; 
-    const thing = new Thing({
-        ...req.body
-    });
-    thing.save()
-        .then(() => res.status(201).json({message: 'Objet enregistré'}))
-        .catch(error => res.status(400).json({ error }));
-});
-
-app.put('/api/stuff/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(thing => res.status(200).json({message: 'Objet modifié'}))
-        .catch(error => res.status(400).json({ error }));
-})
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-        .then(thing => res.status(200).json({message: 'Objet supprimé'}))
-        .catch(error => res.status(400).json({ error }));
-})
-
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-        .then(thing => res.status(200).json(thing))
-        .catch(error => res.status(400).json({ error })); 
-})
+app.use('/api/stuff', stuffRoute); 
 
 
-app.get('/api/stuff', (req, res, next) => {
-    Thing.find()
-        .then(things => res.status(200).json(things))
-        .catch(error => res.status(404).json({ error }));
-});
 
 
 module.exports = app;
