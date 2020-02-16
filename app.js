e1babf6200c2c7ac3eba2cdb62df724e32bc6777
Rémi <ponche62880@gmail.com>
2020-02-16 17:25:58 +0100

    first test with the middleware

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const express = require('express');
 
 const app = express(); 
 
-app.use((req, res) => {
-    res.json({ message: 'Votre requete à bien été reçue :'}); 
+app.use((req, res, next) => {
+    console.log('Requete reçue ! '); 
+    next(); 
 });
+
+app.use((req, res, next) => {
+    res.status(201);
+    next(); 
+});
+
+app.use((req, res, next) => {
+    res.json({ message: 'Votre requete à bien été reçue :'});
+    next();  
+});
+
+app.use((req, res) => {
+    console.log('Réponse envoyé avec succès ! '); 
+})
 
 module.exports = app; 
 
